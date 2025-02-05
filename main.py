5bc70a7b4146c5101138c5baf385be8a2a03326f
Badescu Laurentiu <badescu_laurentiu94@yahoo.com>
2025-02-05 19:37:39 +0200

    Modifications in main.py before merging

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-
+from cart import Cart
 from product import Product
 from product_manager import ProductManager
+import random
 
 def main():
     manager = ProductManager()
     
-    manager.add_product(Product("Laptop", 1200.99, 5))
-    manager.add_product(Product("Mouse", 25.50, 15))
-    manager.add_product(Product("Keyboard", 45.00, 10))
-    manager.add_product(Product("Monitor", 250.75, 7))
+    manager.add_product(Product("Smartphone", 899.99, 10))  
+    manager.add_product(Product("Wireless Mouse", 30.00, 20))  
+    manager.add_product(Product("Mechanical Keyboard", 120.00, 5))  
+    manager.add_product(Product("4K Monitor", 499.99, 8))
     
     print("Available Products: ")
     manager.display_all_products()
@@ -17,3 +18,14 @@ def main():
     print(f"Total Inventory Value: {total_value: .2f}$")
     
     manager.remove_product_by_name("Mouse")
+    
+    cart = Cart()
+    
+    selected_products = random.sample(manager.products, 3)
+    
+    for product in selected_products:
+        quantity = random.randint(1, 3) 
+        cart.add_to_cart(product, quantity)
+        
+    if __name__ == "__main__":
+        main()
